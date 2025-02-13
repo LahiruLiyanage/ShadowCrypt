17f3ba2255430d9470d034e2831c9ef1d384458f
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-13 23:56:44 +0530

    Browsing the files

--- src/main/java/lk/ijse/dep13/shadowcrypt/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/shadowcrypt/controller/MainSceneController.java
@@ -2,6 +2,8 @@ package lk.ijse.dep13.shadowcrypt.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import java.io.*;
 
 public class MainSceneController {
     public Button btnBrowse;
@@ -13,15 +15,31 @@ public class MainSceneController {
     public PasswordField txtPassword;
 
     public void btnBrowseOnAction(ActionEvent event) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        File file = fileChooser.showOpenDialog(btnBrowse.getScene().getWindow());
+        if (file != null) {
+            txtFilePath.setText(file.getAbsolutePath());
+        }
     }
 
     public void btnDecryptOnAction(ActionEvent event) {
+        if (!validateInputs()) return;
 
+        File sourceFile = new File(txtFilePath.getText());
+        if (!sourceFile.getName().endsWith(".encrypted")) {
+            showAlert("Error", "Selected file is not an encrypted file!");
+            return;
+        }
+
+        try {
+            decrypt(sourceFile, txtPassword.getText());
+            showAlert("Success", "File decrypted successfully!");
+            clearFields();
+        } catch (IOException e) {
+            showAlert("Error", "Failed to decrypt file: " + e.getMessage());
+        }
     }
 
-    public void btnEncryptOnAction(ActionEvent event) {
-
-    }
 
 }
