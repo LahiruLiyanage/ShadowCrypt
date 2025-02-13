c44a3407eee58311a5a75dd056503afec66e230b
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-13 23:23:01 +0530

    Update the controller

--- src/main/java/lk/ijse/dep13/shadowcrypt/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/shadowcrypt/controller/MainSceneController.java
@@ -1,4 +1,27 @@
 package lk.ijse.dep13.shadowcrypt.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class MainSceneController {
+    public Button btnBrowse;
+    public Button btnDecrypt;
+    public Button btnEncrypt;
+    public Label lblStatus;
+    public ProgressBar prgProgress;
+    public TextField txtFilePath;
+    public PasswordField txtPassword;
+
+    public void btnBrowseOnAction(ActionEvent event) {
+
+    }
+
+    public void btnDecryptOnAction(ActionEvent event) {
+
+    }
+
+    public void btnEncryptOnAction(ActionEvent event) {
+
+    }
+
 }
